aef2171bc0e006d032faceff792306b2267024d3
Anel <anel.drocic@gmail.com>
2020-02-27 14:02:59 -0600

    quick change to wording of a message

--- warbler-server/handlers/error.js
+++ warbler-server/handlers/error.js
@@ -1,10 +1,10 @@
 // Generic error handler, looks for the 'bubbled up' message and status code otherwise it sets generic 500 and message
-function errorHandler(error, request, response, next) {
+function errorHandler(error, request, response) {
   response.status(error.status || 500);
   response.json({
     error: {
       status: error.status,
-      message: error.message || 'Oops! Something went wrong.',
+      message: error.message || 'Oops! Something went wrong...',
     },
   });
 }
